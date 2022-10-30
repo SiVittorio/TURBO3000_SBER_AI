103837b3741e437144438b9feaf24c5752d0c7b9
SiVittorio <62559964+SiVittorio@users.noreply.github.com>
2022-10-30 10:27:51 +0300

    Create metric.py

---
+++ metric.py
@@ -0,0 +1,33 @@
+#pip install strsimpy
+#pip install easyocr
+
+from strsimpy.weighted_levenshtein import WeightedLevenshtein
+import easyocr
+
+reader = easyocr.Reader(['ru', 'en'])
+
+
+#bounds = reader.readtext("./norm.jpg", paragraph=True) 
+def insertion_cost(char):
+    return 1.0
+
+
+def deletion_cost(char):
+    return 1.0
+
+
+def substitution_cost(char_a, char_b):
+    if char_a == 't' and char_b == 'r':
+        return 0.5
+    return 1.0
+
+weighted_levenshtein = WeightedLevenshtein(
+    substitution_cost_fn=substitution_cost,
+    insertion_cost_fn=insertion_cost,
+    deletion_cost_fn=deletion_cost)
+
+bounds = reader.readtext("./1.jpg", paragraph=True)
+
+for i in range(len(bounds)):
+    print(1 - (weighted_levenshtein.distance(bounds[i][1], '<ИСТИННАЯ СТРОКА>')) /
+               max(len(bounds[i][1]), len('<ИСТИННАЯ СТРОКА>')))
